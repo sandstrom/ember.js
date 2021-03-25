7205240b7987e006eaf3c3cc82b22e71c45a1a42
Alexander Sandström <mail+github@a16m.se>
2021-03-25 22:27:14 +0100

    Deprecate Location.detect
    We're only showing the deprecation warning for locations other
    than 'auto', since auto already have it's own deprecation message.
    
    We're also moving the rootURL trailing slash check from detect
    to initState. That assert has nothing to do with detection, but it
    was likely added there because that method is called *after*
    the rootURL value is set on the location (by the router).
    
    Luckily, the initState method is also called after that value is set,
    so we can use that hook instead.
    
    We're hijacking a method for something that isn't it's primary purpose,
    but that was the case with the existing code too, so this seems
    like a reasonable solution.

--- packages/@ember/-internals/routing/lib/system/router.ts
+++ packages/@ember/-internals/routing/lib/system/router.ts
@@ -894,6 +894,22 @@ class EmberRouter extends EmberObject.extend(Evented) implements Evented {
       // detecting history support. This gives it a chance to set its
       // `cancelRouterSetup` property which aborts routing.
       if (typeof location.detect === 'function') {
+        if (this.location !== 'auto') {
+          deprecate(
+            'The `detect` method on the Location object is deprecated. If you need detection you can run your detection code in app.js, before setting the location type.',
+            false,
+            {
+              id: 'deprecate-auto-location',
+              until: '4.0.0',
+              url: 'https://emberjs.com/deprecations/v3.x#toc_deprecate-auto-location',
+              for: 'ember-source',
+              since: {
+                enabled: '3.28.1',
+              },
+            }
+          );
+        }
+
         location.detect();
       }
 
